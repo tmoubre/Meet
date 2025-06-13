b0284684f5f77cf4947b2e6939b2bbc35566f981
tmoubre <160032580+tmoubre@users.noreply.github.com>
2025-06-13 11:30:46 -0500

    Udated modal

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import { register } from './serviceWorkerRegistration'; 
 import * as atatus from 'atatus-spa';
+import { registerSW } from 'virtual:pwa-register';
 
+// Atatus monitoring
 atatus.config('55d794d255324fc79c3f5150ec0a2ce0').install();
+
+// Register service worker (Vite PWA plugin)
+registerSW({
+  onNeedRefresh() {
+    console.log('🌀 A new version is available. Refresh to update.');
+  },
+  onOfflineReady() {
+    console.log('📡 App is ready to work offline.');
+  }
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-
-register();

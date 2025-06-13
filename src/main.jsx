9f5133e0d35ae8360ef1f03e7adee59af9e5cee1
tmoubre <160032580+tmoubre@users.noreply.github.com>
2025-06-13 09:43:05 -0500

    converting to PWA

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import * as atatus from 'atatus-spa';
+
 atatus.config('55d794d255324fc79c3f5150ec0a2ce0').install();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
+serviceWorkerRegistration.register();

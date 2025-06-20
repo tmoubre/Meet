d3a6188887bf6140aff76cbee78517770a1c0c1d
tmoubre <160032580+tmoubre@users.noreply.github.com>
2025-06-20 05:49:19 -0500

    Updated the setuptest to identify observer

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,21 +1,35 @@
 // src/setupTests.js
 
-
-import '@testing-library/jest-dom';
-
+import "@testing-library/jest-dom";
 
 // Here, add portions of the warning messages you want to intentionally prevent from appearing
 const MESSAGES_TO_IGNORE = [
   "When testing, code that causes React state updates should be wrapped into act(...):",
   "Error:",
-  "The above error occurred"
+  "The above error occurred",
 ];
-
 
 const originalError = console.error.bind(console.error);
 
-
 console.error = (...args) => {
-  const ignoreMessage = MESSAGES_TO_IGNORE.find(message => args.toString().includes(message));
+  const ignoreMessage = MESSAGES_TO_IGNORE.find((message) =>
+    args.toString().includes(message)
+  );
   if (!ignoreMessage) originalError(...args);
-}
+};
+const { ResizeObserver } = window;
+
+beforeEach(() => {
+  //@ts-ignore
+  delete window.ResizeObserver;
+  window.ResizeObserver = jest.fn().mockImplementation(() => ({
+    observe: jest.fn(),
+    unobserve: jest.fn(),
+    disconnect: jest.fn(),
+  }));
+});
+
+afterEach(() => {
+  window.ResizeObserver = ResizeObserver;
+  jest.restoreAllMocks();
+});
